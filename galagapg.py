8d2d0e4b2b0193010a361b3786e2a402ab33f98d
cgosterer <cgo11@my.fsu.edu>
2018-08-01 13:47:02 -0700

    finishing upgrade menu

--- galagapg.py
+++ galagapg.py
@@ -337,9 +337,7 @@ run = True
 posChecker = 1
 pygame.mixer.music.play(loops = -1)
 pygame.mixer.music.set_volume(10.0)
-<<<<<<< HEAD
 gameState = "upgrade"
-#gameState = "Main Menu"
 while run:
 	clock.tick(FPS)
 	if (gameState == "upgrade"):
